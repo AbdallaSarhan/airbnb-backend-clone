7352c087634351c99c93d8469600bd4480685027
Abdalla Sarhan <abdalla1@my.yorku.ca>
2023-10-23 11:06:40 -0400

    Created auth app and added users module with initial controller

---
+++ apps/auth/src/users/users.module.ts
@@ -0,0 +1,20 @@
+import { DatabaseModule, LoggerModule } from '@app/common';
+import { Module } from '@nestjs/common';
+
+import { UserDocument, UserSchema } from './models/user.schema';
+import { UsersController } from './users.controller';
+import { UserRepository } from './users.repository';
+import { UsersService } from './users.service';
+
+@Module({
+  imports: [
+    DatabaseModule,
+    DatabaseModule.forFeature([
+      { name: UserDocument.name, schema: UserSchema },
+    ]),
+    LoggerModule,
+  ],
+  controllers: [UsersController],
+  providers: [UsersService, UserRepository],
+})
+export class UsersModule {}
